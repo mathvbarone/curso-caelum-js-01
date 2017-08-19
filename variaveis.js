9cd87c12f8439ffe585d0f51996648d0df4f7540
Matheus Barone <matheusvbarone@gmail.com>
2017-08-19 20:17:21 -0300

    Variáveis

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+console.log("===VARIÁVEIS===");
+var nome = "Matheus"; //ES5
+let idade = 27; //ES6
+let peso = 80; //ES6
+const sobrenome = "Barone"; //ES6
+const alto = true;
+console.log("peso", peso, typeof peso);
+console.log("sobrenome", sobrenome, typeof sobrenome);
+console.log("alto", alto, typeof alto);
